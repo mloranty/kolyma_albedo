3630a0cc4a9d6081b4eb6de95965dd1edd57fe1c
Mike Loranty <mloranty@colgate.edu>
2020-07-28 14:25:01 -0400

    added a folder for data files, and updated code to deal with raw data

--- kolyma_albedo_analyses.R
+++ kolyma_albedo_analyses.R
@@ -9,10 +9,36 @@
 
 library(raster)
 library(sf)
+library(dplyr)
 
+
+# need to get canopy cover and biomass for each stand to compare with albedo data
 # read in stand data
+setwd("C:/Users/mloranty/Documents/GitHub/kolyma_albedo/")
+
+dg <- read.csv("data/DensityGradientTrees_raw_2010_2017.csv", header = T)
+
+# calculate cumulative biomass for each plot
+dg.bio <- aggregate(dg$Tot.Tree.Carbon.CALC.ALLOM..g.C.m.2.~ dg$Site+dg$Plot,FUN = "sum")
+
+#change header names
+names(dg.bio) <- c("site", "plot", "biomassC")
+
+#define function to calculate standard error of the mean
+se <- function(x, ...) sqrt(var(x, ...)/length(x))
+
+# calculate mean and SE for each stand based on plot sums
+dg.tree <- aggregate(dg.bio$biomassC~dg.bio$site,FUN = "mean")
+                 
+dg.tree$se <- aggregate(dg.bio$biomassC~ dg.bio$site,FUN = "se")[,2]
+
+
+
 # note this is manually collated from Polaris Project Data on ADC and unpublished data from H. Alexander
 tree <- read.csv("C:/Users/mloranty/Documents/GitHub/kolyma_albedo/kolyma_stand_data_FINAL.csv", header = T)
+
+
+
 
 # create spatial points
 tree.sf <- st_as_sf(tree, coords = c("Long", "Lat"), crs = 4326) 
